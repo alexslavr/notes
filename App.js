2da4f78ac082fe3b68d4520b0476a3d28c84c65c
Alex Lavrov <alexander.lavrov@devexpress.com>
2018-08-16 18:40:35 +0300

    Add screens to navigator, connect components to redux store

--- App.js
+++ App.js
@@ -1,23 +1,29 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { createStackNavigator } from 'react-navigation'
+import { Provider } from 'react-redux'
+import List from './components/List'
+import Project from './components/Project'
+import { Note } from './components/Note'
+import { store } from './redux/store'
 
-export default class App extends React.Component {
+const RootStack = createStackNavigator({
+  List: {
+    screen: List
+  },
+  Project: {
+    screen: Project
+  },
+  Note: {
+    screen: Note
+  }
+})
+
+export default class App extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
+      <Provider store={store}>
+        <RootStack/>
+      </Provider>
+    )
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
