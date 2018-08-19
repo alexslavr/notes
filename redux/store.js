eecd46032ba993b15c7264c8e0349855535a711f
alexslavr <alex.s.lavrov@gmail.com>
2018-08-20 01:08:33 +0300

    Add test user avatar image

--- redux/store.js
+++ redux/store.js
@@ -1,33 +1,39 @@
 import { createStore } from 'redux'
 import { reducer } from './reducer'
 
+const userAvatar = require('../images/avatar.jpeg')
+
 export const initialState = {
   projects: [
     {
       id: 'project_1',
-      name: 'NoteList 1',
+      name: 'Project 1',
       notes: [
         {
           id: '1',
           userName: 'Alex',
+          avatar: userAvatar,
           editTime: '11-11-2011',
           text: 'asdfdasfsadf sadfsadf asdfsad sadfas'
         },
         {
           id: '2',
           userName: 'Alex',
+          avatar: userAvatar,
           editTime: '11-11-2011',
           text: 'asdfdasfsadf sadfsadf asdfsad sadfas'
         },
         {
           id: '3',
           userName: 'Alex',
+          avatar: userAvatar,
           editTime: '11-11-2011',
           text: 'asdfdasfsadf sadfsadf asdfsad sadfas'
         },
         {
           id: '4',
           userName: 'Alex',
+          avatar: userAvatar,
           editTime: '11-11-2011',
           text: 'asdfdasfsadf sadfsadf asdfsad sadfas'
         }
@@ -35,29 +41,33 @@ export const initialState = {
     },
     {
       id: 'project_2',
-      name: 'NoteList 2',
+      name: 'Project 2',
       notes: [
         {
           id: '1',
           userName: 'Alex',
+          avatar: userAvatar,
           editTime: '11-11-2011',
           text: 'asdfdasfsadf sadfsadf asdfsad sadfas'
         },
         {
           id: '2',
           userName: 'Alex',
+          avatar: userAvatar,
           editTime: '11-11-2011',
           text: 'asdfdasfsadf sadfsadf asdfsad sadfas'
         },
         {
           id: '3',
           userName: 'Alex',
+          avatar: userAvatar,
           editTime: '11-11-2011',
           text: 'asdfdasfsadf sadfsadf asdfsad sadfas'
         },
         {
           id: '4',
           userName: 'Alex',
+          avatar: userAvatar,
           editTime: '11-11-2011',
           text: 'asdfdasfsadf sadfsadf asdfsad sadfas'
         }
